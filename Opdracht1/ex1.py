4f5609cfc8a21ccae95b458b3df915688e66a659
Micha de Groot <git@michadegroot.nl>
2016-09-13 13:35:50 +0200

    Solved exercise of week 1

--- Opdracht1/ex1.py
+++ Opdracht1/ex1.py
@@ -1,12 +1,12 @@
 # Only one of 11 to 20 made in python 2, wasn't in the mood to rewrite more than a few lines
 import sys
+import fileinput
 
 # Reads the test file and converts it to a 2x2 list
 def readF():
-    f = open('18.txt')
     lineN = 1
     grid = []
-    for line in f:
+    for line in fileinput.input():
         grid.append([])
         for i in range(0,lineN*3,3): 
             element = int(line[i:i+2])
@@ -14,6 +14,32 @@ def readF():
         lineN += 1
     return grid
 
+# Takes as input two consecutive lines of the pyramid 
+# Compares adjacent elements of the lower line and sums it with the element 'above' it in the pyramid
+# Returns a line same length as the upper line of the pyramid
+def newSumLine(upperLine,lowerLine):
+    sumList = []
+    for i in range(len(upperLine)):
+        if(lowerLine[i] > lowerLine[i+1]):
+            sumList.append(lowerLine[i]+upperLine[i])
+        else:
+            sumList.append(lowerLine[i+1]+upperLine[i])
+    return sumList
+
+# Reads the pyramid from the input file
+# Loops through all lines of the pyramid and calculates the highes sum path
+def main():
+    pyramid = readF()
+    pyramidLength = len(pyramid)
+    sumLine = pyramid[pyramidLength-1]
+    for i in range(pyramidLength-2,-1,-1):
+        sumLine = newSumLine(pyramid[i],sumLine)
+    print(sumLine[0])
+
+main() 
+            
+'''
+Old brute force method
 # decrements the index list to the next iteration of indices
 def decrementList(indexList):
     if sum(indexList) <= 0:
@@ -28,12 +54,12 @@ def decrementList(indexList):
                indexList[j+1] = indexList[j] +1 
             break 
     return indexList
-    
+
+
 def main():
     pyramid = readF()
     maxSum = 0
     indexList = list(range(0,len(pyramid)))
-    print(indexList)
     while sum(indexList) > 0:
         pyrSum = 0
         for i in range(0, len(pyramid)):
@@ -41,7 +67,6 @@ def main():
         if pyrSum > maxSum:
             maxSum = pyrSum
         indexList = decrementList(indexList)
-    print(maxSum)
+    print(maxSum, end="")
+'''
 
-main() 
-            
